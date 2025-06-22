1a8d126a43dd14427eddd7f6cf9704044df026ec
Yuriferr <yurifernandespreto@gmail.com>
2025-06-22 16:02:09 -0300

    version 1.3.0

--- Admin/src/components/NavBar/Navbar.jsx
+++ Admin/src/components/NavBar/Navbar.jsx
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css";
-import "./NavBar.css"; // Assuming you have a NavBar.css file
+import { useNavigate, Link } from "react-router-dom";
+import "./NavBar.css";
 import axios from "axios";
 
 import LogoPadrao from "../../images/logo.png";
-import User from "../../images/user.png"; // Importando imagem de usuário padrão
+import User from "../../images/user.png";
 
+// Importando os ícones necessários
 import { CiBoxList } from "react-icons/ci";
-import { IoAnalytics } from "react-icons/io5";
+import { IoAnalytics, IoSettingsOutline, IoLogOutOutline } from "react-icons/io5";
 
 export default function NavBar() {
   const navigate = useNavigate();
-  const [user, setUser] = useState({}); // Simplificado para objeto vazio
+  const [user, setUser] = useState({});
 
   const [empresa, setEmpresa] = useState({
     nomeEmpresa: "Salão Águas Claras",
@@ -27,7 +26,7 @@ export default function NavBar() {
     } else {
       navigate("/login");
     }
-  }, []);
+  }, [navigate]); // Adicionando navigate ao array de dependências
 
   useEffect(() => {
     const API_URL = import.meta.env.VITE_API_URL;
@@ -60,33 +59,37 @@ export default function NavBar() {
       </div>
       <ul>
         <li>
-          <a href="/reviews">
+          {/* Usando Link para navegação interna sem recarregar a página */}
+          <Link to="/reviews">
             <CiBoxList className="icon" />
             <p>Avaliações</p>
-          </a>
+          </Link>
         </li>
         <li>
-          <a href="/dashboard">
+          <Link to="/dashboard">
             <IoAnalytics className="icon" />
             <p>Dashboard</p>
-          </a>
+          </Link>
+        </li>
+        <hr />
+        <li>
+          {/* Link para a página de configurações com ícone apropriado */}
+          <Link to="/">
+            <IoSettingsOutline className="icon" />
+            <p>Configurações</p>
+          </Link>
+        </li>
+        {/* Item da lista que funciona como botão de logout */}
+        <li onClick={handleLogout} style={{ cursor: 'pointer' }}>
+          <IoLogOutOutline className="icon" />
+          <p>Sair</p>
         </li>
       </ul>
-      <Popup
-        trigger={
-          <div className="user-info">
-            {/* You might want to use user.avatar here if available */}
-            <img src={user.avatar || User} alt="Avatar" />
-            <p>{user.email}</p>
-          </div>
-        }
-        position="top center"
-        closeOnDocumentClick
-      >
-        <button className="closeButton" onClick={handleLogout}>
-          Sair
-        </button>
-      </Popup>
+      {/* Mantendo as informações do usuário no final da barra de navegação */}
+      {/* <div className="user-info">
+        <img src={user.avatar || User} alt="Avatar" />
+        <p>{user.email}</p>
+      </div> */}
     </nav>
   );
 }
